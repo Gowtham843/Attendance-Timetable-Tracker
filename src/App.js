e4fd35b7d6cd118b6b95cc5daecb0dfbb8d80aff
Gowtham L <91368589+Gowtham843@users.noreply.github.com>
2026-05-10 12:48:58 +0530

    first committed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/sidebar';
+import Navbar from './components/Navbar';
+import Dashboard from './pages/dashboard';
+import Timetable from './pages/Timetable';
+import Table from './pages/table';
+import { Toaster } from 'react-hot-toast';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Toaster/>
+      <div className="flex flex-col ">
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Sidebar and Main Content */}
+        <div className="flex">
+          <Sidebar />
+
+          {/* Main Content */}
+          <div className="flex-1 bg-gray-100 p-1 ">
+            <Routes>
+              <Route path="/" element={<Timetable />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/table" element={<Table />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
